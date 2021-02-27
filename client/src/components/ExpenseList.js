2007a86bbc6db817dfa397692a969ff311b7c3d0
mikaela.verhoosel <mikaela.verhoosel@gmail.com>
2021-02-27 14:36:13 +1100

    fixed broken dependencies

--- client/src/components/ExpenseList.js
+++ client/src/components/ExpenseList.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
 import CardGroup from "react-bootstrap/CardGroup";
-import './ExpenseList.css'
+import "./ExpenseList.css";
 
 const ExpenseList = () => {
   return (
